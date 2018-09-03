4004afcacb2a2f8bba15d1f3094e429b8c36bc3e
Rich <richard_recar.outlook.com>
2018-09-03 12:44:14 -0400

    'Hello There'

--- newProject/src/app/components/user/user.component.ts
+++ newProject/src/app/components/user/user.component.ts
@@ -7,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
   name:string;
+  age:number;
+  email:string;
+  address: Address;
+  hobbies:string[];
+
   constructor() { }
 
   ngOnInit() {
     this.name = 'John Doe';
+    this.age = 30;
+    this.address = {
+      street:'50 Main st',
+      city: 'Boston',
+      state: 'MA',
+
+    }
+    this.hobbies = ['write code', 'watch movies', 'poop'];
   }
 
+}
+
+interface Address {
+  street:string,
+    city:string,
+    state:string
 }
