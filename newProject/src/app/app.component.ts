661ada35f23d0040837a48dfe5a43665f3e471b5
Rich <richard_recar.outlook.com>
2018-09-03 20:32:57 -0400

    'On my way'

--- newProject/src/app/app.component.ts
+++ newProject/src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector:'pm-root',
+  template: `
+    <div><h1>{{pageTitle}}</h1></div>
+      <div>First Component</div>
+    </div>
+    `
 })
 export class AppComponent {
-  
+  pageTitle: string = 'Rich Page';
 }
